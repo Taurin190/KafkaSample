f75510eb98dd5b27982b7374d1a5cb603d7756fc
Koichi Taura <taura.koichi@gmail.com>
2020-01-23 23:00:51 +0900

    modify typo of docker compose file

--- config/config.go
+++ config/config.go
@@ -7,7 +7,7 @@ type Config struct {
 
 func GetConfig() *Config {
 	return &Config{
-		KafkaServers: []string{"kafkaServers", "localhost:32776", "kafka address"},
+		KafkaServers: []string{"kafkaServers", "localhost:32770", "kafka address"},
 		MongoServer:  []string{"mongoServers", "localhost:27017", "kafka"},
 	}
 }
