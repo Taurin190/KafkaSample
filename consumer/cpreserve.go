2b9cce4a4dcaf911086def87a6752ca240b0bb52
Koichi Taura <taura.koichi@gmail.com>
2020-01-23 23:21:51 +0900

    add consumer to insert message into mongodb

--- consumer/cpreserve.go
+++ consumer/cpreserve.go
@@ -2,9 +2,19 @@ package consumer
 
 import (
 	"fmt"
+	"log"
+	"time"
 
 	"../config"
+	"github.com/globalsign/mgo/bson"
+	"gopkg.in/mgo.v2"
 )
+
+type Message struct {
+	ID        bson.ObjectId `bson:"_id"`
+	Message   string        `bson:"message"`
+	Timestamp string        `bson:"timestamp"`
+}
 
 type consumePreserver struct {
 	Config config.Config
@@ -17,7 +27,31 @@ func GetConsumerPreserver(c config.Config) *consumePreserver {
 }
 
 func (c *consumePreserver) exec(consumedMessage ConsumedMessage) {
-	fmt.Println(fmt.Sprintf("consumed message. message: %s, timestamp: %d", consumedMessage.Message, consumedMessage.Timestamp))
+	mongoInfo := &mgo.DialInfo{
+		Addrs:    []string{c.Config.MongoServer[1]},
+		Timeout:  20 * time.Second,
+		Database: c.Config.MongoServer[2],
+		Username: "kafka",
+		Password: "kafka",
+		Source:   "kafka",
+	}
+
+	session, err := mgo.DialWithInfo(mongoInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+	db := session.DB(c.Config.MongoServer[2])
+
+	mes := &Message{
+		ID:        bson.NewObjectId(),
+		Message:   consumedMessage.Message,
+		Timestamp: fmt.Sprintf("%d", consumedMessage.Timestamp),
+	}
+	col := db.C("message")
+	if err := col.Insert(mes); err != nil {
+		log.Fatalln(err)
+	}
 }
 
 func (c *consumePreserver) Run() {
